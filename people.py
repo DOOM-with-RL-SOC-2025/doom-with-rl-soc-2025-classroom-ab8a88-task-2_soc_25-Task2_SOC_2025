51112272d41963833d256fcd2c5cf475242cb63e
Rohit <rohitrajkumar.1810@gmail.com>
2025-06-04 17:00:00 +0530

    Changed both files

--- people.py
+++ people.py
@@ -20,7 +20,7 @@ class Employee:
     name : str 
     age : int
     ID : int
-    city : int
+    city : str #should be string right ???
     branches : list[int] # This is a list of branches (as branch codes) to which the employee may report
     salary : int 
 
@@ -28,10 +28,10 @@ class Employee:
                  branchcodes, salary = None):
         self.name = name
         self.age = age 
-        self.ID = ID
+        self.ID = int(ID)
         self.city = city
         self.branches = branchcodes
-        if salary is not None: self.salary = salary
+        if salary is not None and salary != "": self.salary = int(salary)
         else: self.salary = 10_000 
     
     def change_city(self, new_city:str) -> bool:
@@ -48,7 +48,7 @@ class Employee:
         # branch to report to. Fail for others.
         # Change old branch to new if it is in the same city, else return false.
         if len(self.branches) == 1 and self.branches[0] != new_code:
-            if branchmap[self.branches[0]]["city"] == branchmap[new_code]["city"]:
+            if branchmap[int(self.branches[0])]["city"] == branchmap[new_code]["city"]:
                 self.branches[0] = new_code
                 return True
         return False
@@ -73,7 +73,10 @@ class Engineer(Employee):
         pstn = ["Junior","Senior","Team Lead","Director"]
         if position in pstn:
             self.position = position
+        else:
+            self.position = "Junior"
         # Only then set the position. 
+    #set the position to lowest if nothing or wrong given
 
     
     def increment(self, amt:int) -> None:
@@ -81,17 +84,16 @@ class Engineer(Employee):
         # and increment to an engineer's salary should add a 10% bonus on to "amt"
         self.salary += amt*1.1
         
-    def promote(self, position:str) -> bool:
+    def promote(self) -> bool:
         # Return false for a demotion or an invalid promotion
         # Promotion can only be to a higher position and
         # it should call the increment function with 30% of the present salary
         # as "amt". Thereafter return True.
         pstn = ["Junior","Senior","Team Lead","Director"]
-        if position in pstn:
-            if pstn.index(self.position) < pstn.index(position):
-                self.increment(0.3*self.salary)
-                self.position = position
-                return True
+        if pstn.index(self.position) < 3:
+            self.increment(0.3*self.salary)
+            self.position = pstn[pstn.index(self.position)+1]
+            return True
         return False
 
 class Salesman(Employee):
@@ -105,7 +107,7 @@ class Salesman(Employee):
     that the employee reports to. This argument should be the ID of the superior
     It should be None for a "Head" and so, the argument should be optional in init.
     """
-    
+    position : int  #dont we need this ???
     # An extra member variable!
     superior : int # EMPLOYEE ID of the superior this guy reports to
 
@@ -115,16 +117,19 @@ class Salesman(Employee):
         pstn = ["Rep","Manager","Head"]
         if position in pstn:
             self.position = position
+        else:
+            self.position = "Rep"
         self.superior = superior
+        #if wrong position is given then its being set to lowest
     
     # def promote
-    def promote(self, position:str) -> bool:
+    #changing the promote function to promote to next position
+    def promote(self) -> bool:
         pstn = ["Rep","Manager","Head"]
-        if position in pstn:
-            if pstn.index(self.position) < pstn.index(position):
-                self.increment(0.3*self.salary)
-                self.position = position
-                return True
+        if pstn.index(self.position) < 2:
+            self.increment(0.3*self.salary)
+            self.position = pstn[pstn.index(self.position)+1]
+            return True
         return False 
 
     # def increment
@@ -135,23 +140,23 @@ class Salesman(Employee):
         # Return the employee ID and name of the superior
         # Report a tuple of None, None if no superior.
         if self.superior == None:
-            return [None,None]
+            return (None,None)
         for sales in sales_roster:
             if self.superior == sales.ID:
-                return [sales.ID,sales.name]
-        return [None,None]
+                return (sales.ID,sales.name)
+        return (None,None)
 
-    def add_superior(self) -> bool:
+    def add_superior(self,sup_ID) -> bool:
         # Add superior of immediately higher rank.
         # If superior doesn't exist return false,
         pstn = ["Rep","Manager","Head"]
-        if self.superior == None and self.position != "Head":
+        #can add if superior present since promotion changes superior
+        if self.position != "Head":
             for sales in sales_roster:
-                if sales.position == pstn[pstn.index(self.position)+1]:
+                if sales.ID == sup_ID and sales.position == pstn[pstn.index(self.position)+1]:
                     self.superior = sales.ID
                     return True
         return False
-
 
     def migrate_branch(self, new_code: int) -> bool:
         # This should simply add a branch to the list; even different cities are fine
